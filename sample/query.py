81489ec1a3771137c8dcad08b8d754960830cd8b
Sapir Shamai <118911764+SapirShamai@users.noreply.github.com>
2023-02-08 14:37:12 +0100

    replaced the data file and edited the code.

--- sample/query.py
+++ sample/query.py
@@ -1,20 +1,23 @@
-from data import warehouse1, warehouse2
+from data import stock
 
-name = input("Please enter your name: ")
-print(f"Hallo, {name.capitalize()}!")
+name = input("Please enter your name: ").capitalize()
+print(f"Hallo, {name}!")
 menu = """1. List items by warehouse
 2. Search an item and place an order
 3. Quit"""
 number = int(input(f"What would you like to do: \n{menu}\nType the number of the operation: "))
 
 if number == 1:
-    print("### Warehouse1 Items ###")
-    for item in warehouse1:
-        print(item)
-
-    print("### Warehouse2 Items ###")
-    for item in warehouse2:
-        print(item)
+    print("\n### Warehouse 1 : ###\n")
+    for i in stock:
+        items = i["state"] + " " + i["category"]
+        if i["warehouse"] == 1:
+            print(items)
+    print("\n### Warehouse 2 : ###\n")
+    for i in stock:
+        items = i["state"] + " " + i["category"]
+        if i["warehouse"] == 2:
+            print(items)
 
     print(f"Thank you for your visit, {name}!")
     quit()
@@ -23,57 +26,52 @@ if number == 1:
 elif number == 2:
     x = 0
     while x == 0:
-        item_name = input("What is the name of the item? ")
+        item_name = input("What is the name of the item? ").lower()
+        amount = 0
+        for item in stock:
+            if (item["state"] + " " + item["category"]).lower() == item_name:
+                amount += 1
+
+        print(f"Amount available: {amount}")
         amount1 = 0
-        for item in warehouse1:
-            if item == item_name:
-                amount1 += 1
         amount2 = 0
-        for item in warehouse2:
-            if item == item_name:
+
+        for item in stock:
+            if (item["state"] + " " + item["category"]).lower() == item_name and item["warehouse"] == 1:
+                amount1 += 1
+            elif (item["state"] + " " + item["category"]).lower() == item_name and item["warehouse"] == 2:
                 amount2 += 1
-
         total_amount = amount1 + amount2
-        print(f"Amount available: {total_amount}")
-
-        if item_name in warehouse1 and item_name not in warehouse2:
-            print("Location: Warehouse 2")
-
-        elif item_name in warehouse1 and item_name in warehouse2:
-            print("Location: both warehouses")
-            if amount1 > amount2:
-                print(f"In Warehouse 1 the amount is higher. amount: {amount1}")
-            elif amount1 == amount2:
-                print(f"The amount of items is the same in both warehouses: {amount1}")
-            else:
-                print(f"In Warehouse 2 the amount is higher. amount: {amount2}")
-
-
-        elif item_name in warehouse2 and item_name not in warehouse1:
-            print("Location: Warehouse 2")
+        if amount1 > amount2:
+            print(f"In Warehouse 1 the amount is higher. amount: {amount1}")
+        elif amount1 == amount2 and amount1 != 0:
+            print(f"The amount of items is the same in both warehouses: {amount1}")
+        elif amount2 > amount1:
+            print(f"In Warehouse 2 the amount is higher. amount: {amount2}")
         else:
-            answer = input(f"Not in stock\nWould you like to search for another item?").lower()
+            answer = input(f"Not in stock\nWould you like to search for another item?  Y/N").lower()
             if answer == "y" or answer == "yes":
                 continue
             else:
                 x += 1
 
         if total_amount > 0:
-            answer = input(f"Would you like to place an order for {item_name}? ").lower()
+            answer = input(f"Would you like to place an order for {item_name}?  Y/N ").lower()
             if answer == "y" or answer == "yes":
                 how_many = int(input(f"How many {item_name}s would you like? "))
                 if 0 < how_many <= total_amount:
                     print(f"Your order: {how_many} {item_name} is placed!")
-                    answer = input("Would you like to continue shopping?").lower()
+                    answer = input("Would you like to continue shopping?  Y/N").lower()
                     if answer == "y" or answer == "yes":
                         continue
                     else:
                         x += 1
                 else:
-                    answer = input(f"Error!\nThe maximus available is: {total_amount} items, would you like to take it instead? ").lower()
+                    answer = input(
+                        f"Error!\nThe maximus available is: {total_amount} items, would you like to take it instead?  Y/N").lower()
                     if answer == "y" or answer == "yes":
                         print(f"Your order: {total_amount} {item_name} is placed!")
-                        answer = input("Would you like to continue shopping?").lower()
+                        answer = input("Would you like to continue shopping?  Y/N").lower()
                         if answer == "y" or answer == "yes":
                             continue
                         else:
@@ -82,8 +80,6 @@ elif number == 2:
                         x += 1
             else:
                 x += 1
-
-
 
 
 elif number == 3:
